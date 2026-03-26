6885e383add98340093a124c1480984c744e1c3f
Infoagnayi <info.agnayi@gmail.com>
2026-03-26 17:33:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 import sqlite3, os, csv, io, random
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 app.secret_key = "change-me-in-production"
 DB_PATH = os.path.join(os.path.dirname(__file__), "crm.db")
 
